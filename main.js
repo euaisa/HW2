333c3baf5d48e2b9c8b61ce6e0bd3fcb2673699c
veraexequiel <veraexequiel5@gmail.com>
2023-01-16 11:35:23 -0800

    average functionality done

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 document.addEventListener("DOMContentLoaded", function (event) {
 
-    document.getElementById("button1").addEventListener("click", function () {
+    document.getElementById("averagebtn").addEventListener("click", function () {
+let num1 = parseFloat(document.getElementById("num1").value);
+let num2 = parseFloat(document.getElementById("num2").value);
+let num3 = parseFloat(document.getElementById("num3").value);
 
-        document.getElementById("textbox1").value = "success";
+        document.getElementById("result").innerHTML = (num1 + num2 + num3)/3;
     });
 });
 
